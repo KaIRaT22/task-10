3c55e89d1d513b2bc942e4b91dc80bc93d497583
Кайрат Давранбеков <kdavranbekov@gmail.com>
2020-12-12 09:11:39 +0600

    second push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Get from './Get';
+import Post from './Post';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+
+class App extends React.Component {
+
+    render() {
+
+        return (
+
+            <Router>
+                <div className='wrapper'>
+                    <h1>React tutorial</h1>
+                    <ul>
+                        <li>
+                            <Link to="/get">GET</Link>
+                        </li>
+                        <li>
+                            <Link to="/post">POST</Link>
+                        </li>
+                    </ul>
+                </div>
+                <Switch>
+                    <Route path={'/get'} component={ Get }></Route>
+                    <Route path={'/post'} component={ Post }></Route>
+                </Switch>
+            </Router>
+          )
+    }
 }
 
 export default App;
